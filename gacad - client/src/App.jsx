63eed2073cf589eb63fd4523c4c38c224a6bfc4f
DozaPat <gacadaaron@gmail.com>
2026-04-10 22:52:04 +0800

    Revert "Merge pull request #1 from DozaPat/lab-act2"
    This reverts commit 6b73b86f284bd6a514eee4698b6b9b8313dba1a7, reversing
    changes made to 985f4b5649a3f47bbf662dc446ac98620f0c17ab.

--- gacad - client/src/App.jsx
+++ gacad - client/src/App.jsx
@@ -1,39 +1,26 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
 
-import Layout from './components/Layout';
-import ArticlePage from './pages/ArticlePage';
-import HomePage from './pages/HomePage';
-import AboutPage from './pages/AboutPage';
+function App() {
+  const [count, setCount] = useState(0)
 
-const routes = [
-  {
-    path: '/',
-    element: <Layout />,
-    children: [
-      {
-        path: '',
-        element: <HomePage />,
-      },
-      {
-        path: 'about',
-        element: <AboutPage />,
-      },
-      {
-        path: 'articles',
-        element: <ArticlePage />,
-      },
-    ],
-  },
-];
-
-const router = createBrowserRouter(routes);
-
-function App(){
   return (
     <>
-      <RouterProvider router={router} />
+      <div>
+        <h1>Welcome daddy</h1>
+        <p>
+          Name: Marc Aaron M. Gacad<br></br>
+          Section: INF 234<br></br>
+          Student ID: 2023-102496<br></br>
+          Status: Single<br></br>
+        </p>
+        <a href="https://github.com/DozaPat/gacad-webprog">"https://github.com/DozaPat/gacad-webprog"</a>
+      </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
