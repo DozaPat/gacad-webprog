71f8b10a74513847cc5d36da07bc7d908d5ff862
DozaPat <gacadaaron@gmail.com>
2026-06-03 22:51:35 +0800

    kung di gagana to unfriend ko si munoz

--- gacad-server/index.js
+++ gacad-server/index.js
@@ -18,7 +18,7 @@ app.use(cors({
     'http://localhost:5173',
     'http://127.0.0.1:5173',
     'https://gacad-backend.onrender.com',           // your Render backend (for testing)
-    'https://gacad-webprog-gu84wfyj9-doza-s-projects.vercel.app',      // ← We will replace this later
+    'https://gacad-webprog-git-lab-act7-doza-s-projects.vercel.app',      // ← We will replace this later
     
 
   ],
